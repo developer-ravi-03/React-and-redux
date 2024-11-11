02c128d8361df6beeb2fba42baf8bdc833085d98
Ravi Kumar <ravibseb96@gmail.com>
2024-11-11 23:09:38 +0530

    Calculator version 2
    completed calculator version 2

---
+++ Projects/5-Calculator-version-2.0/src/components/Display.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import styles from "./Display.module.css";
+const Display = ({ displayValue }) => {
+  return (
+    <input
+      type="text"
+      value={displayValue}
+      readOnly
+      className={styles.display}
+    />
+  );
+};
+export default Display;
