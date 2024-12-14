0f5f62ef831fb8fec77df26b64ad103a21e37d34
Ravi Kumar <ravibseb96@gmail.com>
2024-12-14 16:07:39 +0530

    Myntra project completed
    I have completed my react redux course and i have completed myntra project as well as

---
+++ Projects/9-Myntra-Clone/myntra-clone/src/components/BagEmpty.jsx
@@ -0,0 +1,9 @@
+const BagEmpty = () => {
+  return (
+    <center className="bg-body-tertiary p-5 rounded">
+      <h1>Cart is Empty</h1>
+    </center>
+  );
+};
+
+export default BagEmpty;
---
+++ Projects/9-Myntra-Clone/myntra-clone/src/routes/Bag.jsx
@@ -0,0 +1,34 @@
+import BagEmpty from "../components/BagEmpty";
+import BagItem from "../components/BagItem";
+import BagSummary from "../components/BagSummary";
+import { useSelector } from "react-redux";
+
+const Bag = () => {
+  const bagItems = useSelector((state) => state.bag);
+  const items = useSelector((state) => state.items);
+  const finalItems = items.filter((item) => {
+    const itemIndex = bagItems.indexOf(item.id);
+    return itemIndex >= 0;
+  });
+
+  const bagItemsfetch = bagItems.length;
+
+  return (
+    <main>
+      {bagItemsfetch !== 0 ? (
+        <div className="bag-page">
+          <div className="bag-items-container">
+            {finalItems.map((item) => (
+              <BagItem key={item.id} item={item} />
+            ))}
+          </div>
+          <BagSummary />
+        </div>
+      ) : (
+        <BagEmpty />
+      )}
+    </main>
+  );
+};
+
+export default Bag;
